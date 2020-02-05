e5cd9aef3a1195c9cd67a7c0553f8e206e44198b
kanav9307 <200419307@student.georgianc.on.ca>
2020-02-04 18:54:49 -0600

    Lab test

---
+++ bash/login-ssh.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#
+# This script produces a dynamic welcome message
+# it should look like
+#   Welcome to planet hostname, title name!
+
+# Task 1: Use the variable $USER instead of the myname variable to get your name
+# Task 2: Dynamically generate the value for your hostname variable using the hostname command - e.g. $(hostname)
+# Task 3: Add the time and day of the week to the welcome message using the format shown below
+#   Use a format like this:
+#   It is weekday at HH:MM AM.
+# Task 4: Set the title using the day of the week
+#   e.g. On Monday it might be Optimist, Tuesday might be Realist, Wednesday might be Pessimist, etc.
+#   You will need multiple tests to set a title
+#   Invent your own titles, do not use the ones from this example
+###############
+# Variables   #
+###############
+myname="$USER"
+hostname="$(hostname)"
+day=$(date +%A)
+currenttime=$(date +%I:%M\ %p)
+###############
+# Main        #
+###############
+if [ "$day" = Monday ]
+then
+a="Welcome to planet $hostname, Super $myname!"
+elif [ "$day" = Tuesday ]
+then
+a="Welcome to planet $hostname, Hacker $myname!"
+elif [ "$day" = Wednesday ]
+then
+a="Welcome to planet $hostname, Nerd $myname!"
+elif [ "$day" = Thursday ]
+then
+a="Welcome to planet $hostname, Great $myname!"
+elif [ "$day" = Friday ]
+then
+a="Welcome to planet $hostname, Avatar $myname!"
+elif [ "$day" = Saturday ]
+then
+a="Welcome to planet $hostname, Betman $myname!"
+else
+a="Welcome to planet $hostname, Spiderman $myname!"
+fi
+b="It is $currenttime on $day ."
+cowsay "$a $b"
